b9384428ab5f00fc473712d0cdaf06159b737ae3
Amanda1024 <88503704+Amanda1024@users.noreply.github.com>
2021-08-25 17:03:01 -0700

    starts Pokemon Character List array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-alert('Hello world');
-
-const favoriteFood = 'Spaghetti🍝';
-document.write(favoriteFood);
+//Pokemon Character List
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison'],
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        type: ['grass', 'poison'],
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        type: ['grass', 'poison'],
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: ['fire'],
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        type: ['fire'],
+    }
+];
